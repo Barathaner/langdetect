3a83710fe03e4a80c2ece0983749605f48c380d6
Karl-Augustin Jahnel <freelance@karlaugustinjahnel.de>
2024-02-29 02:45:48 +0100

    added some more tokenizers

--- LangDetect/source/preprocess.py
+++ LangDetect/source/preprocess.py
@@ -1,15 +1,22 @@
 import pandas as pd
 import nltk
-from nltk.corpus import stopwords
-import re
 import MeCab
 import pythainlp
-from konlpy.tag import Okt  # Okt ist ein Tokenizer für Koreanisch
-
-import jieba    
-
-# Diese Funktion bleibt unverändert
+from konlpy.tag import Okt  # Okt is a tokenizer for Korean.
+import jieba
+from cltk import NLP
+cltk_nlp = NLP(language="lat")
+nltk.download('punkt')
 def tokenize_japanese(text):
+    """
+    Tokenizes Japanese text using MeCab.
+
+    Parameters:
+    - text (str): The Japanese text to be tokenized.
+
+    Returns:
+    - list: A list of tokens extracted from the input text.
+    """
     tokenizer = MeCab.Tagger()
     tokens = []
     node = tokenizer.parseToNode(text)
@@ -19,31 +26,67 @@ def tokenize_japanese(text):
         node = node.next
     return tokens
 
-# Verbesserte Tokenisierung für Chinesisch
 def tokenize_chinese(text):
-    # Entfernen von Sonderzeichen vor der Tokenisierung
+    """
+    Tokenizes Chinese text using Jieba.
+
+    Parameters:
+    - text (str): The Chinese text to be tokenized.
+
+    Returns:
+    - list: A list of tokens extracted from the input text.
+    """
     tokens = jieba.cut(text)
     return list(tokens)
 
+
 def tokenize_thai(text):
-    # Verwende pythainlp zum Tokenisieren des Thai-Textes
+    """
+    Tokenizes Thai text using PyThaiNLP.
+
+    Parameters:
+    - text (str): The Thai text to be tokenized.
+
+    Returns:
+    - list: A list of tokens extracted from the input text.
+    """
     tokens = pythainlp.tokenize.word_tokenize(text)
     return tokens
 
 def tokenize_korean(text):
-    # Verwende konlpy's Okt zum Tokenisieren des Koreanisch-Textes
+    """
+    Tokenizes Korean text using Okt from konlpy.
+
+    Parameters:
+    - text (str): The Korean text to be tokenized.
+
+    Returns:
+    - list: A list of tokens extracted from the input text.
+    """
     okt = Okt()
     tokens = okt.morphs(text)
     return tokens
 
 
 def preprocess(sentence, labels):
+    """
+    Preprocesses text data for various languages by applying language-specific tokenization.
+
+    Parameters:
+    - sentence (pd.Series): A pandas Series containing sentences to be preprocessed.
+    - labels (pd.Series): A pandas Series containing labels indicating the language of each sentence.
+
+    Returns:
+    - tuple: A tuple containing two pandas Series: the preprocessed sentences and the original labels.
+
+    The function supports preprocessing for Chinese, Japanese, Thai, and Korean texts. For languages not specifically supported, it defaults to simple whitespace-based tokenization. This preprocessing step is crucial for natural language processing tasks, especially when dealing with languages that do not use whitespace to separate words.
+    """
     corpus = []
     sentence = sentence.to_list()
     labels = labels.to_list()
+    
 
     for i in range(len(sentence)):
-        # Entfernen von Sonderzeichen und Umwandlung in Kleinbuchstaben
         processed_sentence = sentence[i]
 
         if labels[i] == 'Chinese':
@@ -54,11 +97,31 @@ def preprocess(sentence, labels):
             tokens = tokenize_thai(processed_sentence)
         elif labels[i] == 'Korean':
             tokens = tokenize_korean(processed_sentence)
+        elif labels[i] == 'Tamil':
+            tokens=processed_sentence.split()
+        elif labels[i] == 'Urdu':
+            tokens=processed_sentence.split()
+        elif labels[i] == 'Persian':
+            tokens=processed_sentence.split()
+        elif labels[i] == 'Pushto':
+            tokens=processed_sentence.split()
+        elif labels[i] == 'Romanian':
+            tokens=processed_sentence.split()
+        elif labels[i] == 'Arabic':
+            tokens=processed_sentence.split()
+        elif labels[i] == 'Hindi':
+            tokens=processed_sentence.split()
+        elif labels[i] == 'Latin':
+            cltk_doc = cltk_nlp.analyze(text=processed_sentence)
+            tokens=cltk_doc.tokens
+        elif labels[i] == 'Indonesian':
+            tokens=processed_sentence.split()
         else:
-            # Standardverhalten für andere Sprachen
-            tokens = processed_sentence.split()
+            lang = labels[i].lower()
+            if lang == 'portugese':
+                lang = 'portuguese'
+            tokens = nltk.tokenize.word_tokenize(text=processed_sentence,language=lang)
         
-        # Wiederzusammenfügen der tokenisierten Wörter zu einem Satz
         processed_sentence = ' '.join(tokens)
         corpus.append(processed_sentence)
 
